315094f090e7e2defeba972de6fbf3e90238a068
Sora Morimoto <sora@morimoto.io>
2025-01-23 15:11:55 +0900

    TailwindCSS v4
    Signed-off-by: Sora Morimoto <sora@morimoto.io>

--- src/App.res
+++ src/App.res
@@ -1,10 +1,12 @@
 @react.component
 let make = () => {
-  <>
-    <h1> {"ReScript Vercel Speed Insights Example"->React.string} </h1>
-    <p>
-      <b> {"IMPORTANT"->React.string} </b>
+  <div className="p-2">
+    <h1 className="font-bold text-2xl">
+      {"ReScript Vercel Speed Insights Example"->React.string}
+    </h1>
+    <p className="text-base">
+      <span className="font-semibold"> {"IMPORTANT"->React.string} </span>
       {": Open the console!"->React.string}
     </p>
-  </>
+  </div>
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,17 @@
 import * as process from "node:process";
 import rescript from "@jihchi/vite-plugin-rescript";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   base: process.env.CI ? "/rescript-vercel-speed-insights-example/" : "/",
-  build: {
-    cssMinify: "lightningcss",
-  },
-  css: {
-    transformer: "lightningcss",
-  },
   define: {
     "import.meta.env.VERCEL_ANALYTICS_ID": JSON.stringify(
       process.env.VERCEL_ANALYTICS_ID,
     ),
     "import.meta.env.VERCEL_ENV": JSON.stringify(process.env.VERCEL_ENV),
   },
-  plugins: [rescript({ silent: true })],
+  plugins: [tailwindcss(), rescript({ silent: true })],
   server: {
     port: 3000,
   },
